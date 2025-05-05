c77b06e7af73a3eb679748357537481cf87277ff
Shivam5531 <Shivamkaushik12344321@gmail.com>
2025-05-05 12:30:29 +0530

    Update pp.py

--- pp.py
+++ pp.py
@@ -180,5 +180,6 @@ def restart():
 # --- Run First Time ---
 restart_process()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+
